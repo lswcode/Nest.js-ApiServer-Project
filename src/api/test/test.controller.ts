5bb9c65c5cc7839fbd3438760e505c9bd49ac9b8
lsw <809568994@qq.com>
2021-12-01 22:23:17 +0800

    12.1

--- src/api/test/test.controller.ts
+++ src/api/test/test.controller.ts
@@ -1,12 +1,19 @@
+/* eslint-disable prettier/prettier */
 import { Controller, Get } from '@nestjs/common';
 
 @Controller('test')
 export class TestController {
-  @Get('get')
+  @Get('xb')
   public getFun() {
-    return {
-      name: '测试小白',
-      age: 24,
-    };
+    return [
+      {
+        name: '测试小白',
+        age: 24,
+      },
+      {
+        name: '测试小红',
+        age: 27,
+      },
+    ];
   }
 }
