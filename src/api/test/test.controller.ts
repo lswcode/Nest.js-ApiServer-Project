6c325b5e42f742b55233b1ef9ba597d64fa72858
lsw <809568994@qq.com>
2021-12-13 11:52:13 +0800

    12.13

--- src/api/test/test.controller.ts
+++ src/api/test/test.controller.ts
@@ -1,6 +1,15 @@
 /* eslint-disable prettier/prettier */
 import { Controller, Get } from '@nestjs/common';
-
+const obj = [
+  {
+    name: '测试小白',
+    age: 24,
+  },
+  {
+    name: '测试小红',
+    age: 27,
+  },
+];
 @Controller('test')
 export class TestController {
   @Get('xb')
@@ -15,5 +24,19 @@ export class TestController {
         age: 27,
       },
     ];
+  }
+
+  @Get('jsonpTest')
+  public returnCallbackFun() {
+    return `resCallback([
+      {
+        name: '测试小白',
+        age: 24,
+      },
+      {
+        name: '测试小红',
+        age: 27,
+      },
+    ])`;
   }
 }
